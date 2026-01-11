2b95293e1ab6859e9f1e29309476002c564d205f
moukthika <moukthikagvs@gmail.com>
2026-01-11 15:23:58 +0530

    Auto-load weather data on first startup

--- weather/apps.py
+++ weather/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
 
-
 class WeatherConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'weather'
+
+    def ready(self):
+        from .models import WeatherData
+        if not WeatherData.objects.exists():
+            from .utils import load_weather_data
+            load_weather_data()
